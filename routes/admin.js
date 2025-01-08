84e00598e7769db1487bb6e9b97856949d55e809
jhfidelis <fidelis.henrique.jh@gmail.com>
2025-01-07 22:10:37 -0300

    Cadastrando categorias no banco de dados

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,9 @@
 // Carregando módulos
 const express = require('express'); // Constante para receber o express
 const router = express.Router(); // Constante para gerar rota em arquivo diferente do 'app.js'
+const mongoose = require('mongoose'); // Constante para receber o mongoose
+require('../models/Categoria'); // Carrega o modelo de dados da Categoria para uso no arquivo
+const Categoria = mongoose.model('categorias'); // Constante para acessar a coleção 'categorias' no banco de dados
 
 // Definido rotas
     // Rota principaldo administrador
@@ -21,6 +24,22 @@ const router = express.Router(); // Constante para gerar rota em arquivo diferen
     // Rota para adicionar categorias
     router.get('/categorias/add', (req, res) => {
         res.render('admin/add-categoria');
+    });
+
+    // Rota para efetuar o registro de uma nova categoria
+    router.post('/categorias/nova', (req, res) => {
+        // Constante para receber os dados do formulário
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        };
+
+        // Salvando os dados obtido em novaCategoria no BD
+        new Categoria(novaCategoria).save().then(() => {
+            console.log("Categoria cadastrada com sucesso!");
+        }).catch((err) => {
+            console.log("Erro ao cadastrar categoria: " + err);
+        });
     });
 
 module.exports = router; //Exportanto constante para permitir o acesso de outros arquivos
