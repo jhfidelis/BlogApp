8085190ddb6654c876f2a32ceb3c1e5b92603172
jhfidelis <fidelis.henrique.jh@gmail.com>
2025-01-11 01:10:45 -0300

    Validando formulário de edição de categoria

--- routes/admin.js
+++ routes/admin.js
@@ -90,24 +90,49 @@ const Categoria = mongoose.model('categorias'); // Constante para acessar a cole
 
     // Rota para aplicar as edições feitas pela categoria
     router.post('/categorias/edit', (req, res) => {
-        console.log("ID recebido:", req.body.id); // Depuração do ID enviado
-        Categoria.findOne({_id: req.body.id}).then((categoria) => {
-            categoria.nome = req.body.nome;
-            categoria.slug = req.body.slug;
 
-            categoria.save().then(() => {
-                req.flash('success_msg', "Categoria editada com sucesso!");
-                res.redirect('/admin/categorias');
+         // Validando formulário
+         var erros = [];
+
+         if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+             erros.push({texto: "Nome inválido"});
+         }
+         if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+             erros.push({texto: "Slug inválido"});
+         }
+         if (req.body.nome.length < 2) {
+             erros.push({texto: "Nome da categoria é muito pequeno"});
+         }
+ 
+         if (erros.length > 0) {
+            // Retornando os valores salvos no formulário
+            res.render('admin/edit-categoria', {
+                erros: erros,
+                categoria: {
+                    _id: req.body.id,
+                    nome: req.body.nome,
+                    slug: req.body.slug
+                }
+            });
+        }
+
+         else {
+            Categoria.findOne({_id: req.body.id}).then((categoria) => {
+                categoria.nome = req.body.nome;
+                categoria.slug = req.body.slug;
+    
+                categoria.save().then(() => {
+                    req.flash('success_msg', "Categoria editada com sucesso!");
+                    res.redirect('/admin/categorias');
+                }).catch((err) => {
+                    req.flash('error_msg', "Houve um erro interno ao salvar a edição da categoria!");  
+                    res.redirect('/admin/categorias');
+                });
             }).catch((err) => {
-                console.error("Erro ao salvar:"+ err); // Depuração do erro
-                req.flash('error_msg', "Houve um erro interno ao salvar a edição da categoria!");  
+                req.flash('error_msg', "Houve um erro ao editar a categoria");
                 res.redirect('/admin/categorias');
             });
-        }).catch((err) => {
-            console.error("Erro ao buscar categoria:"+ err); // Depuração do erro
-            req.flash('error_msg', "Houve um erro ao editar a categoria");
-            res.redirect('/admin/categorias');
-        });
+         }
     });
 
 module.exports = router; //Exportanto constante para permitir o acesso de outros arquivos
