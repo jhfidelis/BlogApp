97b2a65df82f5406e60b80c893e81d179751632a
jhfidelis <fidelis.henrique.jh@gmail.com>
2025-02-13 00:36:16 -0300

    Implementando a edição de postagem

--- routes/admin.js
+++ routes/admin.js
@@ -214,4 +214,46 @@ const Postagem = mongoose.model('postagens'); // Constante para acessar a coleç
         }
     });
 
+    // Rota para redirecionar para página de edição de postagem
+    router.get('/postagens/edit/:id', (req, res) => {
+        // Recuperar o conteúdo da postagem selecionada
+        Postagem.findOne({_id: req.params.id}).lean().then((postagem) => {
+            // Recuperar as categorias no banco de dados
+            Categoria.find().lean().then((categorias) => {
+                res.render('admin/edit-postagem', {categorias: categorias, postagem: postagem});
+            }).catch((err) => {
+                req.flash('error_msg', "Houve um erro ao listar as categorias");
+                res.redirect('/admin/postagens');
+            });
+
+        }).catch((err) => {
+            req.flash('error_msg', "Houve um erro ao carregar o formulário de edição de postagem");
+            res.redirect('/admin/postagens');
+        });
+   });
+
+   // Rota para aplicar as edições feitas pela categoria
+   router.post('/postagens/edit', (req, res) => {
+    // Buscando uma postagem que tenha o id igual ao que foi passado na requisição
+    Postagem.findOne({_id: req.body.id}).then((postagem) => {
+        postagem.titulo = req.body.titulo;
+        postagem.slug = req.body.slug;
+        postagem.descricao = req.body.descricao;
+        postagem.conteudo = req.body.conteudo;
+        postagem.categoria = req.body.categoria;
+
+        postagem.save().then(() => {
+            req.flash('success_msg', "Postagem editada com sucesso");
+            res.redirect('/admin/postagens');
+        }).catch((err) => {
+            req.flash('error_msg', "Erro interno");
+            res.redirect('/admin/postagens');
+        });
+    }).catch((err) => {
+        console.log(err);
+        req.flash('error_msg', "Houve um erro ao salvar a edição.");
+        res.redirect('/admin/postagens');
+    });
+   });
+
 module.exports = router; //Exportanto constante para permitir o acesso de outros arquivos
